fd36f659b8193e9fef9421027242162a28d39f74
shangugregory <shangugregory@gmail.com>
2022-05-07 21:33:49 +0300

    3rd day

--- app/src/main/java/com/example/actioneersapp/LogIn.java
+++ app/src/main/java/com/example/actioneersapp/LogIn.java
@@ -4,25 +4,55 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class LogIn extends AppCompatActivity {
     
      TextView register;
+     EditText logInEmail, logInPasssword;
+     Button logInBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
+        logInBtn=(Button) findViewById(R.id.log_in_btn);
+        logInEmail = (EditText) findViewById(R.id.log_in_email);
+        logInPasssword= (EditText) findViewById(R.id.log_in_user_password);
         register= (TextView) findViewById(R.id.txtRegister);
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(LogIn.this, Registration.class);
                 startActivity(intent);
+
             }
         });
+
+
+        logInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = logInEmail.getText().toString().trim();
+                String passwrd = logInPasssword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    logInEmail.setError("Email Is Required");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(passwrd)){
+                    logInPasssword.setError("Password is Required");
+                    return;
+                }
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/actioneersapp/Registration.java
+++ app/src/main/java/com/example/actioneersapp/Registration.java
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class Registration extends AppCompatActivity {
 TextView logIn;
 Button register;
+EditText rgEmail, rgfName, rgLname,rgOtherName, rgAge, rgId, rgPwrd, rgConPwrd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,5 +29,76 @@ Button register;
 
             }
         });
+
+        rgEmail = (EditText) findViewById(R.id.register_user_Email);
+        rgfName = (EditText) findViewById(R.id.register_first_name);
+        rgLname = (EditText) findViewById(R.id.register_last_name);
+        rgOtherName=(EditText) findViewById(R.id.register_other_name);
+        rgAge = (EditText) findViewById(R.id.register_age);
+        rgId = (EditText) findViewById(R.id.register_id_no);
+        rgPwrd=(EditText) findViewById(R.id.register_user_password);
+        rgConPwrd=(EditText) findViewById(R.id.register_confirm_user_password);
+
+
+
+        register = (Button) findViewById(R.id.register_btn);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String rEmail = rgEmail.getText().toString().trim();
+                String rFname = rgfName.getText().toString().trim();
+                String rLname = rgLname.getText().toString().trim();
+                String rOName = rgOtherName.getText().toString().trim();
+                String rAge = rgAge.getText().toString().trim();
+                String rPwd = rgPwrd.getText().toString().trim();
+                String rConPwrd = rgConPwrd.getText().toString().trim();
+
+
+                if(TextUtils.isEmpty(rEmail)){
+                    rgEmail.setError("Email is Required");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(rFname)){
+                    rgfName.setError("First Name Can not be empty");
+                    return;
+                }
+                if (TextUtils.isEmpty(rLname)){
+                    rgLname.setError("Last name can not be empty");
+                    return;
+                }
+                if (TextUtils.isEmpty(rAge)){
+                    rgAge.setError("Age Can Not Empty");
+                    return;
+                }
+                if (Double.parseDouble(rAge)>18){
+                    rgAge.setError("You have To be atleast over 18 years to Participate");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(rPwd)){
+                    rgPwrd.setError("PassWord Can not be Empty");
+                    return;
+                }
+                if (TextUtils.isEmpty(rConPwrd)){
+                    rgConPwrd.setError("Please Confirm your Password");
+                    return;
+                }
+                if (rConPwrd != rConPwrd){
+                    rgPwrd.setError("Password Missmartch");
+                    return;
+                }
+
+
+            }
+        });
+
+
+
+
+
+
+
+
     }
 }
